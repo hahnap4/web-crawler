3caf6f1ec4fdbc09f4d4aa1ee302057e01cb6cec
Nina Hahn <hahnap4@gmail.com>
2023-01-06 18:19:50 -0600

    Added the command lines in main.js

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log('hello world');
+function main() {
+    console.log()
+    if (process.argv.slice(2).length < 1) {
+        console.error("Error: Less than 1 argument given");
+        process.argv.exit;
+    } else if (process.argv.slice(2).length > 1) {
+        console.error("Error: More than 1 argument given");
+        process.argv.exit;
+    } else {
+        console.log("The crawler is starting at " + process.argv[2]);
+    }
+}
+
+main()
