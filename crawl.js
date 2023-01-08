16e0df7d5446ef5957022c93629f3a0495069f52
Nina Hahn <hahnap4@gmail.com>
2023-01-07 21:11:09 -0600

    Added print report function and crawl page function

--- crawl.js
+++ crawl.js
@@ -12,26 +12,80 @@ function normalizeURL(url){
 }
 
 function getURLsFromHTML(htmlBody, baseURL) {
+    try{
     const dom = new JSDOM(htmlBody, {
         includeNodeLocations: true,
     });
     const document = dom.window.document;
     const links = document.querySelectorAll('a');
-    let hrefArray = [];
+
+    const hrefArray = [];
 
     for (let i = 0; i < links.length; i++) {
-        if (links[i].href.startsWith('h')) {
-            let url = links[i].href;
-            hrefArray.push(url);
-        } else {
-            let url = links[i].href;
-            let normUrl = baseURL+url;
-            hrefArray.push(normUrl);
-        }
+        let url = links[i].href;
+
+                if(url.includes('http') === false){
+                    url = baseURL + url;
+                }
+
+                hrefArray.push(url);
     }
 
     return hrefArray;
+} catch (err) {
+    throw new Error(err);
+}
+}
+
+async function crawlPage(baseURL, currentURL, pages) {
+            const currentUrlObj = new URL(currentURL);
+            const baseUrlObj = new URL(baseURL);
+            if (currentUrlObj.hostname !== baseUrlObj.hostname) {
+            return pages;
+            }
+
+            const normalizedURL = normalizeURL(currentURL);
+            if (pages[normalizedURL] > 0){
+                pages[normalizedURL]++;
+                console.log(`Added +1 to ${pages[normalizedURL]}`);
+                return pages;
+            } 
+            
+            pages[normalizedURL] = 1;
+
+            let html = '';
+
+            try{
+            console.log(`starting fetch for ${currentURL}...`);
+            const response = await fetch(currentURL);
+        
+            if (response.status > 399) {
+                console.error(response.status);
+                return pages;
+            }
+            
+            console.log('crawling starting - ' + currentURL);
+    
+            const contentType = response.headers.get('content-type');
+            if (contentType.includes('text/html') === false) {
+                console.error(`Error: invalid content-type - ${contentType}. Need text/html instead.`);
+                return pages;
+            }
+    
+            html = await response.text();
+            } catch (e) {
+                console.error(e);
+            } 
+
+            const urlArray = getURLsFromHTML(html, baseURL); 
+            
+            for (const url of urlArray){
+                    pages = await crawlPage(baseURL, url, pages);
+            }
+
+            return pages;
 }
 
 exports.normalizeURL = normalizeURL;
 exports.getURLsFromHTML = getURLsFromHTML;
+exports.crawlPage = crawlPage;
--- main.js
+++ main.js
@@ -1,14 +1,19 @@
-function main() {
-    console.log()
+const { crawlPage } = require('./crawl.js');
+const { printReport } = require('./report.js');
+
+async function main() {
     if (process.argv.slice(2).length < 1) {
         console.error("Error: Less than 1 argument given");
         process.argv.exit;
-    } else if (process.argv.slice(2).length > 1) {
+    } 
+    
+    if (process.argv.slice(2).length > 1) {
         console.error("Error: More than 1 argument given");
         process.argv.exit;
-    } else {
-        console.log("The crawler is starting at " + process.argv[2]);
     }
+        console.log("The crawler is starting at " + process.argv[2]);
+        const pages = await crawlPage(process.argv[2], process.argv[2], {});
+        printReport(pages);
 }
 
 main()
